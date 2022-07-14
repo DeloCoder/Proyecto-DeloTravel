51fef8ab8950bc83ec5805c099445e560a2a93fd
Diego Del Hoyo <104804654+DeloCoder@users.noreply.github.com>
2022-07-14 08:57:19 -0300

    Delete funciones.js

--- funciones.js
+++
@@ -1,8 +0,0 @@
-function solicitudAyuda() {
-    document.getElementById("solicitudAyuda").innerHTML =
-      "Un agente se comunicara con Ud a la brevedad";
-    console.log("ATENCION!!! El cliente solicita ayuda en la pagina");
-  }
-  document.getElementById("botonDeAyuda").onclick = function ()
-      { solicitudAyuda();};
-  
